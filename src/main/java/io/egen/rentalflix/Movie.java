17ce182791c63fcf72da7c6ae7645d4a4522aedb
= <=>
2016-02-13 13:30:54 -0600

    feat(Movie):- implemented movie entities

--- src/main/java/io/egen/rentalflix/Movie.java
+++ src/main/java/io/egen/rentalflix/Movie.java
@@ -6,4 +6,61 @@ package io.egen.rentalflix;
  */
 public class Movie {
 	//POJO IMPLEMENTATION GOES HERE
+	
+	private int id;
+	private String title;
+	private int year;
+	private String language;
+	private boolean isRented;
+	
+	
+	public Movie(int id, String title, int year, String language) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.year = year;
+		this.language = language;
+	}
+	
+	public int getId()
+	{
+		return id;
+	}
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+	public String getTitle()
+	{
+		return title;
+	}
+	public void setTitle(String title)
+	{
+		this.title = title;
+	}
+	public int getYear()
+	{
+		return year;
+	}
+	public void setYear(int year)
+	{
+		this.year = year;
+	}
+	public String getLanguage()
+	{
+		return language;
+	}
+	public void setLanguage(String language)
+	{
+		this.language = language;
+	}
+	public boolean isRent()
+	{
+		return isRent;
+	}
+
+	public void setRent(boolean isRent)
+	{
+		this.isRent = isRent;
+	}
 }
